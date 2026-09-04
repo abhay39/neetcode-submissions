9c94c479d8f026acb31b60bdd777833cb2e83f05
Abhay Kumar Gupta <84112755+abhay39@users.noreply.github.com>
2026-09-04 14:24:42 +0530

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,12 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buy_at = prices[0]
-        result = 0
+        max_profit = 0
 
         for i in range(1, len(prices)):
+
+            # Can I buy cheaper today?
             if prices[i] < buy_at:
                 buy_at = prices[i]
-            else:
-                result = max(result, prices[i] - buy_at)
 
-        return result
+            # If I sell today, what's my profit?
+            profit = prices[i] - buy_at
+
+            # Remember the best profit
+            max_profit = max(max_profit, profit)
+
+        return max_profit
