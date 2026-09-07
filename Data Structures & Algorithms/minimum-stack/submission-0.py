5684f5127ef659a647f72b3d9ee1e5d3e18b1e16
Abhay Kumar Gupta <84112755+abhay39@users.noreply.github.com>
2026-09-07 15:56:43 +0530

    Update: minimum-stack - submission-0

--- Data Structures & Algorithms/minimum-stack/submission-0.py
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -1,17 +1,25 @@
 class MinStack:
 
     def __init__(self):
-        self.stack=[]
+        self.item=[]
+
+    def isEmpty(self):
+        return len(self.item)==0
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        return 
+        self.item.append(val)
+
     def pop(self) -> None:
-        self.stack.pop()
+        if not self.isEmpty():
+            self.item.pop()
+        else:
+            raise IndexError("pop from empty stack")
 
     def top(self) -> int:
-        return self.stack[-1]
+        if not self.isEmpty():
+            return self.item[-1]
+        else:
+            raise IndexError("peek from empty stack")
 
     def getMin(self) -> int:
-        temp=sorted(self.stack)
-        return temp[0]
+        return min(self.item)
