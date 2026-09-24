9277b60c5e88c6cdf02e0adad1eba988b5ff23ae
Abhay Kumar Gupta <84112755+abhay39@users.noreply.github.com>
2026-09-24 22:43:54 +0530

    Update: lru-cache - submission-0

--- Data Structures & Algorithms/lru-cache/submission-0.py
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -1,22 +1,27 @@
+from collections import OrderedDict
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.lru = {}
         self.capacity = capacity
+        self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key not in self.lru:
+        if key not in self.cache:
             return -1
-
-        value = self.lru.pop(key)      # Remove
-        self.lru[key] = value          # Reinsert at the end
-        return value
+        
+        # Move the accessed item to the end (marks it as most recently used)
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        if key in self.lru:
-            self.lru.pop(key)          # Remove old position
-
-        self.lru[key] = value          # Insert at end
-
-        if len(self.lru) > self.capacity:
-            self.lru.pop(next(iter(self.lru)))   # Remove least recently used
+        # If key exists, moving it to the end updates its recency status
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            
+        # Insert or update the value
+        self.cache[key] = value
+        
+        # If we exceeded the capacity, remove the least recently used item (the first one)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
